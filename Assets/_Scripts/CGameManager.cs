9423051c57484556257838beef9965892329ab47
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-07-10 12:38:44 +0200

    Création classe singleton GameManager + test singleton GameManager avec instance >1 TODO DontDestroyOnLoad test scene multiple+ gameManager

---
+++ Assets/_Scripts/CGameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CGameManager : MonoBehaviour
+{
+    public static CGameManager _instance;
+
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+            
+
+    }
+
+    public void initializeScene()
+    {
+        //Charger les données persistantes game state données
+        //Charger le system et dépendences 
+
+        Debug.Log("Loading...");
+    }
+}
